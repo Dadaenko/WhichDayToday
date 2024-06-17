40c729d656906589279380d3c6ea97519c4dfafe
Daria Makarenko <darya.makarenko@gmail.com>
2024-06-17 23:13:07 +0300

    Changes to js file

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-In ac pulvinar ligula, laoreet sodales dolor. Vestibulum pellentesque dignissim eros, sed pulvinar urna efficitur non. Duis semper arcu ut odio efficitur fringilla. Nunc id justo mollis, tempus est ac, cursus dolor. Etiam porta interdum erat, ut egestas ligula euismod a. Donec condimentum erat vel urna tempus, at aliquam odio semper. Maecenas consectetur venenatis nunc ac vestibulum. Ut a feugiat ante. Morbi quis varius neque, blandit molestie orci.
+import express from "express";
 
-//hello here, world
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  //Test code: new Date("June 24, 2023 11:13:00");
+  const day = today.getDay();
+
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if (day === 5 || day === 6) {
+    {
+      type = "the weekend";
+      adv = "it's time to have some fun";
+    }
+  }
+
+  // this object is passed to the index.ejs file
+  res.render("index.ejs", { dayType: type, advice: adv });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
